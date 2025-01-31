c49c285bea12728d3ab92a4f512b3263fe798ab3
Andrey Zakharov <mediaexplorer74@hotmail.com>
2025-01-31 10:50:07 +0300

    .

---
+++ README.md
@@ -0,0 +1,18 @@
+# Deepseek - console branch
+![Logo](Images/logo.png)
+
+Experimental Deepseek project... simplest console app for DeepSeek API micro-testin'... 
+## Status
+- DeepSeek generated this c# code. Not me :)
+- Azure.AI.Inference (prerelease) package used.
+- Draft / proto / not ready yet.
+
+## References
+- status.deepseek.com (Status of DeepSeek project)
+- deepseek.com (Registration & API Key "getting")
+
+## ..
+As is. No support. RnD only. DIY.
+
+## .
+[m][e] 2025
---
+++ Src/DeepseekConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//dotnet add package Azure.AI.Inference --prerelease
+using Azure;
+using Azure.AI.Inference;
+
+
+namespace DeepseekConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // To authenticate with the model you will need to generate a personal access token (PAT) in your GitHub settings. 
+            // Create your PAT token by following instructions here: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
+            AzureKeyCredential credential = new AzureKeyCredential
+            (
+              "xxxxxx"
+            );
+
+            ChatCompletionsClient client = new ChatCompletionsClient(
+                new Uri("https://models.inference.ai.azure.com"),
+                credential,
+                new ChatCompletionsClientOptions());
+
+            var requestOptions = new ChatCompletionsOptions()
+            {
+                Messages =
+                {
+                    new ChatRequestUserMessage(
+                        "Can you explain the basics of machine learning?"),
+                },
+                Model = "DeepSeek-R1",
+                MaxTokens = 2048,
+
+            };
+
+            Response<ChatCompletions> response = client.Complete(requestOptions);
+            var rcontent = response.Value.Choices[0].Message.Content;
+            System.Console.WriteLine(rcontent);//.Choices[0].Message.Content);
+            Debug.WriteLine("Response content: " + rcontent);
+
+        }
+    }
+}
